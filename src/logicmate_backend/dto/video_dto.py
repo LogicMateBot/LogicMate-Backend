5512505f8990436c946d760ab7c10f381528979f
NightmareVCO <vladimircuriel@outlook.com>
2025-07-28 00:07:13 -0400

    refactor(video): update VideoRequestDTO and VideoResponseDTO to remove Optional fields and enhance type consistency

--- src/logicmate_backend/dto/video_dto.py
+++ src/logicmate_backend/dto/video_dto.py
@@ -1,28 +1,55 @@
-from typing import List, Optional
+from typing import List
 from bson import ObjectId
+from fastapi import UploadFile
 from pydantic import BaseModel, ConfigDict, Field, field_validator
 from logicmate_backend.models.video import Scene
 
 
 class VideoRequestDTO(BaseModel):
+    id: str
     duration: str
-    categories: Optional[List[str]] = Field(default=None)
+    categories: List[str]
     scenes: List[Scene]
-    explanation: Optional[str] = Field(default=None)
+    title: str
+    explanation: str
+    code: str
+    diagram: str
+    approaches: List
+    exercises: List
+    users: List
 
 
 class VideoResponseDTO(BaseModel):
     model_config = ConfigDict(populate_by_name=True, from_attributes=False)
 
-    id: Optional[str] = Field(alias="_id")
+    id: str = Field(alias="_id")
     duration: str
-    categories: Optional[List[str]] = Field(default=None)
-    scenes: List[Scene]
-    explanation: Optional[str] = Field(default=None)
+    categories: List[str]
+    scenes: List
+    title: str
+    explanation: str
+    code: str
+    diagram: str
+    approaches: List
+    exercises: List
+    users: List
 
     @field_validator("id", mode="before")
     @classmethod
     def _convert_object_id(cls, v):
         if isinstance(v, ObjectId):
-            return str(object=v)
+            return str(v)
         return v
+
+
+class ProcessVideoRequestDTO(BaseModel):
+    file: UploadFile = Field(default=..., description="MP4 video file to process")
+    users_emails: List[str] = Field(
+        default=..., description="List of user emails to notify"
+    )
+    current_user_email: str = Field(
+        default=..., description="Email of the current user"
+    )
+
+    class Config:
+        arbitrary_types_allowed = True
