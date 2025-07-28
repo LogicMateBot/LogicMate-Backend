d430d419b201190892748ccd55b39bfb713b0871
NightmareVCO <vladimircuriel@outlook.com>
2025-07-28 00:07:47 -0400

    feat(docker): add Redis service and update db_studio port mapping refactor(pyproject): update dependencies to specific versions and add new packages feat(main): create video directory and configure CORS middleware

--- src/logicmate_backend/main.py
+++ src/logicmate_backend/main.py
@@ -1,13 +1,16 @@
 # uvicorn logicmate_backend.main:app --app-dir src --reload
 
+import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from logicmate_backend.config.db import get_main_db
 from logicmate_backend.controllers.user_controller import router as user_router
 from logicmate_backend.controllers.auth_controller import router as auth_router
 from logicmate_backend.controllers.video_controller import router as video_router
 from pymongo.errors import PyMongoError
 from dotenv import load_dotenv
+from pathlib import Path
 
 
 @asynccontextmanager
@@ -20,12 +23,24 @@ async def lifespan(app: FastAPI):
     yield
 
 
+VIDEO_DIR = Path("videos")
+VIDEO_DIR.mkdir(parents=True, exist_ok=True)
+
+
 load_dotenv()
 app = FastAPI(
     title="LogicMate Backend",
     description="LogicMate Backend API, a RESTful API for LogicMate",
     version="1.0.0",
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv(key="FRONT_END_URL", default="*")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
